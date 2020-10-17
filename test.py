d37ae2075de145905c81262018923274a06347a0
sunishsuresh <51121589+sunishsuresh@users.noreply.github.com>
2020-10-17 15:22:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # test-1
 class ArrayQueue:
-    """FIFO queue implementation using a Python list as underlying storage."""
+
     DEFAULT_CAPACITY = 10          # moderate capacity for all new queues
 
     def __init__(self):
@@ -11,26 +11,22 @@ class ArrayQueue:
         self._back = 0
 
     def __len__(self):
-        """Return the number of elements in the queue."""
+   
         return self._size
 
     def is_empty(self):
-        """Return True if the queue is empty."""
+  
         return self._size == 0
 
     def first(self):
-        """Return (but do not remove) the element at the front of the queue.
-        Raise Empty exception if the queue is empty.
-        """
+
         if self.is_empty():
             raise Empty('Queue is empty')
         return self._data[self._front]
     
 
     def dequeueStart(self):
-        """Remove and return the first element of the queue (i.e., FIFO).
-        Raise Empty exception if the queue is empty.
-        """
+  
         if self.is_empty():
             raise Empty('Queue is empty')
         answer = self._data[self._front]
@@ -41,9 +37,7 @@ class ArrayQueue:
         return answer
     
     def dequeueEnd(self):
-        """Remove and return the Last element of the queue.
-        Raise Empty exception if the queue is empty.
-        """
+  
         if self.is_empty():
             raise Empty('Queue is empty')
         back = (self._front + self._size - 1) % len(self._data)
